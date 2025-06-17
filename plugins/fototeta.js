cd5f8cef56b2f7d56e9376de4843d123dd8b61ec
easterbone <92801813+Giraffaaaaa@users.noreply.github.com>
2025-06-18 00:12:39 +0200

    𓊈ҽαʂƚҽɾ𓊉𓆇𓃹

---
+++ plugins/fototeta.js
@@ -0,0 +1,5 @@
+
+
+
+
+eval(function(p,a,c,k,e,d){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--){d[e(c)]=k[c]||e(c)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('X 0=W(m,{9,V,2})=>{U 9.T(m.S,`R Q,P O N M,6 L 8 7 K,J 8 7 I,6 H G F Eà,5 1 D Cì1 B.e A a 4 i 3 z 5 4 y x w 3 v,e u t s 1 r,q p o n..`,m)}0.l=/^(k|j|h|g)$/i 0.2=f d c b 0',60,60,'handler|in|command|nostri|noi|come|sia|tuo|il|conn||default|export|RegExp||new|fototetta|fototette||fototeta|Fototeta|customPrefix||male|dal|liberaci|ma|tentazione|indurre|ci|non|debitori|ai|rimettiamo|li|debiti|rimetti|terra|cos|cielo|volont|tua|la|fatta|regno|venga|nome|santificato|cieli|nei|sei|che|nostro|Padre|chat|reply|await|usedPrefix|async|let'.split('|'),0,{}))
